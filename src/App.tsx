7eeb8474bf37d6ac023a00db01236f675ba41a02
TanPassapol <tanreddevils@gmail.com>
2026-01-05 15:43:19 +0700

    feat: add dark/light mode and skills

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,32 @@ function App() {
     .catch(err => console.error(err));
   }, [])
 
+  // Light/Dark Mode
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+  };
+  useEffect(() => {
+    document.body.className = theme
+  }, [theme]);
+
+  // Skills
+  const [skills, setSkills] = useState(['React', 'TypeScript']);
+  const [newSkill, setNewSkill] = useState("");
+  
+  const addSkill = () => {
+    if (newSkill.trim() !== ""){
+      setSkills([...skills,newSkill]);
+      setNewSkill("");
+    } 
+  };
+
   return (
-    <div style = {{textAlign: 'center'}}>
+    <div className={theme} style={{ textAlign: 'center', minHeight: '100vh', width: '500px' }}>
         <h1>My Team Portfolio</h1>
+        <button onClick={toggleTheme}>
+          Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+        </button>
 
         {githubData ? (
           <ProfileCard
@@ -27,6 +50,18 @@ function App() {
         ) : (
           <p>Loading data from GitHub...</p>
         )}
+
+        <input style = {{marginTop: '20px'}}
+        value = {newSkill}
+        onChange = {(e) => setNewSkill(e.target.value)}
+        placeholder = "Add a skill"
+        />
+        <button onClick = {addSkill}>Add Skill</button>
+        <ul>
+          {skills.map((skill, index) => (
+            <li style={{margin: '5px', textAlign: 'left'}} key = {index}>{skill}</li>
+          ))}
+        </ul>
     </div>
   );
 }
