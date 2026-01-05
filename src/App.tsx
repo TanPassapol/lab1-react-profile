021aafed8aa1223a65ac424e113390ec2ac9a6ad
TanPassapol <tanreddevils@gmail.com>
2026-01-05 14:40:55 +0700

    feat: implement counter state and github api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import ProfileCard from "./components/ProfileCard"
+import { useState, useEffect } from 'react'; 
 
 function App() {
+  const [githubData, setGithubData] = useState<any>(null);
+  const username = "TanPassapol"
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+    .then(data => {
+      setGithubData(data);
+    })
+    .catch(err => console.error(err));
+  }, [])
+
   return (
-    <div>
+    <div style = {{textAlign: 'center'}}>
         <h1>My Team Portfolio</h1>
 
-        <ProfileCard
-          name = "Passapol Lukthongkum"
-          role = "Student @ CEDT"
-          bio = "21"
-        />
-
-        <ProfileCard
-          name = "John Doe"
-          role = "Guest Developer"
-          bio = "I love coding and learning new things."
-        />
+        {githubData ? (
+          <ProfileCard
+            name = {githubData.name || githubData.login}
+            role = "GitHub User"
+            bio = {githubData.bio || "No bio avaliable"}
+          />
+        ) : (
+          <p>Loading data from GitHub...</p>
+        )}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type ProfileCardDetails = {
   name: string
   role: string
@@ -5,11 +7,17 @@ type ProfileCardDetails = {
 }
 
 function ProfileCard({name, role, bio}: ProfileCardDetails) {
+    const [likes, setLikes] = useState(0);
+
     return(
         <div style = {{border: '1px solid #ccc', padding: '20px', borderRadius: '10px'}}>
             <h2>{name}</h2>
             <p><strong>Role:</strong> {role}</p>
             <p>{bio}</p>
+
+            <button onClick = {() => setLikes(likes + 1)}>
+                Like: {likes}
+            </button>
         </div>
     )
 }
